fdaff26f66822607dbdd13575ad54c9f3289bf1c
Ian Voysey <iev@cs.cmu.edu>
2017-03-05 20:33:02 -0500

    pylint

--- th.py
+++ th.py
@@ -1,30 +1,31 @@
-from flask import Flask
 import sys
+from flask import Flask, request
 
-BASE="/home/vagrant/tests/out/" + sys.argv[0]
-file = open(BASE + '/th.log', 'w')
+app = Flask(__name__)
+BASE = "/home/vagrant/tests/out/" + sys.argv[0]
+F = open(BASE + '/th.log', 'w')
 
 @app.route("/ready", methods=["POST"])
 def ready():
     req = request.get_json(silent=True)
-    file.write(str(req))
+    F.write(str(req))
     lock = open(BASE + "thlock", 'w')
     lock.close()
 
 @app.route("/error", methods=["POST"])
 def error():
     req = request.get_json(silent=True)
-    file.write(str(req))
+    F.write(str(req))
 
 @app.route("/status", methods=["POST"])
 def status():
     req = request.get_json(silent=True)
-    file.write(str(req))
+    F.write(str(req))
 
 @app.route("/done", methods=["POST"])
 def done():
     req = request.get_json(silent=True)
-    file.write(str(req))
+    F.write(str(req))
 
 if __name__ == "__main__":
     app.run("0.0.0.0") ## probably wrong, it'll clash with
